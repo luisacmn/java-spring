ee2698782bd227be15c8c9eedfb0baee4de6a9d5
luisacmn <luisanovaes.arq@gmail.com>
2024-01-20 20:24:53 -0300

    add: create candidate table

--- gestao_vagas/src/main/java/br/com/luisanovaes/gestao_vagas/modules/candidate/CandidateEntity.java
+++ gestao_vagas/src/main/java/br/com/luisanovaes/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,9 +1,14 @@
 package br.com.luisanovaes.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -12,6 +17,8 @@ import lombok.Data;
 @Data
 public class CandidateEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;    
     private String name;
 
@@ -24,6 +31,9 @@ public class CandidateEntity {
     
     @Length(min = 10, max = 100, message = "A senha deve conter entre 10 e 100 caracteres")
     private String password;
+    private String description;
     private String curriculum;
-    
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;   
 }
